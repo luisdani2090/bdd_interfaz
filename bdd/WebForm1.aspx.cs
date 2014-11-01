a3b558149fdc214919dff9d29677a97c5a08ac07
Daniel Aguiar <luisdani2086@gmail.com>
2014-11-01 14:46:40 -0600

    Initial Commit

---
+++ bdd/WebForm1.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace bdd
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            using (var context = new bdd_ejemploEntities())
+            {
+                //var custQuery =
+                //from cust in context.Customers
+                //select cust;
+                //GridView1.DataSource = context.Customers.ToList();
+                //int i = 0;
+                
+                //context.Customers_33.Add(new Customers_33 { Name = "Luis" });
+                //context.SaveChanges();
+                GridView1.DataSource = context.Customers.ToList();
+                GridView1.DataBind();
+                GridView3.DataSource = context.Customers_33.ToList();
+                GridView3.DataBind();                
+            }
+            using (var context2 = new bdd_ejemploEntities2())
+            {
+                GridView2.DataSource = context2.Customers_66.ToList();
+                GridView2.DataBind();
+            }
+            GridView1.Visible = true;
+        }
+    }
+}
