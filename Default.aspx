009bf4c5e47bdccc30cddea75e36694b7cd241cf
cniackz <cesar.celis.h@gmail.com>
2014-11-04 09:18:01 -0600

    Adding the colored basic interface to the web app branch.

--- Default.aspx
+++ Default.aspx
@@ -9,6 +9,13 @@
     <style type="text/css">
         .auto-style1 {
             font-size: large;
+            text-align: left;
+        }
+        .auto-style2 {
+            font-size: large;
+        }
+        .auto-style3 {
+            height: 319px;
         }
     </style>
 </head>
@@ -16,50 +23,79 @@
     <form id="form1" runat="server">
     <div>
     
-        <strong><span class="auto-style1">Server3 and Server4 </span></strong>
-        <br />
-        <br />
-        1. Display tests<br />
+        <table style="width:100%;">
+            <tr>
+                <td class="auto-style3"><span class="auto-style1">
     
-        <asp:GridView ID="GridView1" runat="server" OnSelectedIndexChanged="GridView1_SelectedIndexChanged" style="width: 187px">
+        <strong>Server3 and Server4 
+                    <br />
+                    <br />
+                    </strong>
+        1. Display tests<br />
+                    <br />
+    
+        <asp:GridView ID="GridView1" runat="server" ondatabound="GridView1_DataBound" onrowdatabound="GridView1_RowDataBound" OnSelectedIndexChanged="GridView1_SelectedIndexChanged" style="width: 187px">
         </asp:GridView>
     
-        <br />
+                    <br />
         <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Refresh Table" />
-        <br />
-        <br />
-        <strong><span class="auto-style1">Server3</span></strong><br />
-        <br />
-        1. Insert new test<br />
+                    </span></td>
+                <td class="auto-style3"><span class="auto-style1">
+        <strong>Server3<br />
+                    <br />
+                    </strong>1. Insert new test<strong><br />
         <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
         <br />
         <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Insert Server3" />
+                    <br />
         <br />
-        Result:
+                    </strong>Result:
         <asp:Label ID="Label1" runat="server"></asp:Label>
-        <br />
-        <br />
+                    <br />
+                    <br />
+                    <br />
         2. Delete a test<br />
         <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
         <br />
         <asp:Button ID="Button5" runat="server" OnClick="Button5_Click" Text="Delete Server3" />
+                    <br />
         <br />
         Result:
         <asp:Label ID="Label2" runat="server"></asp:Label>
         <br />
-        <br />
+                    </span></td>
+                <td class="auto-style3">
         <strong><span class="auto-style1">Server4</span></strong><br />
         <br />
-        1. Insert new test<br />
+                    <span class="auto-style2">1. Insert new test</span><br />
         <asp:TextBox ID="TextBox2" runat="server" OnTextChanged="TextBox2_TextChanged"></asp:TextBox>
         <br />
-        <asp:Button ID="Button3" runat="server" Text="Insert Server4" />
+        <asp:Button ID="Button3" runat="server" Text="Insert Server4" OnClick="Button3_Click" />
+                    <br />
+                    <br />
+                    <span class="auto-style2">Result:</span><asp:Label ID="Label3" runat="server"></asp:Label>
         <br />
+                    <br />
         <br />
-        2. Delete a test<br />
+                    <span class="auto-style2">2. Delete a test</span><br />
         <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
         <br />
-        <asp:Button ID="Button4" runat="server" Text="Delete Server4" />
+        <asp:Button ID="Button4" runat="server" Text="Delete Server4" OnClick="Button4_Click" />
+                    <br />
+                    <br />
+                    <span class="auto-style2">Result:</span><asp:Label ID="Label4" runat="server"></asp:Label>
+        <br />
+    
+                </td>
+            </tr>
+        </table>
+        <br />
+        <asp:Label ID="Label5" runat="server"></asp:Label>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
         <br />
     
     </div>
--- Default.aspx.cs
+++ Default.aspx.cs
@@ -23,10 +23,6 @@ public partial class _Default : System.Web.UI.Page
     {
 
     }
-    protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
-    {
-
-    }
     protected void Button1_Click(object sender, EventArgs e)
     {
         using (var context = new db_testsEntities_server4())
@@ -53,6 +49,7 @@ public partial class _Default : System.Web.UI.Page
                 context.server3_table_tests.Add(test_row);
                 context.SaveChanges();
                 Label1.Text = "Succeded";
+                Label5.Text = "";
                 using (var context_2 = new db_testsEntities_server4())
                 {
                     ///////////////////////////////////////////////////////////////////
@@ -64,7 +61,8 @@ public partial class _Default : System.Web.UI.Page
             }
             catch (Exception exep) 
             {
-                Label1.Text = "Fail to insert: "+exep;
+                Label1.Text = "Failed";
+                Label5.Text = "Fail to insert: "+exep;
             }
 
         }
@@ -75,7 +73,7 @@ public partial class _Default : System.Web.UI.Page
     }
     protected void Button5_Click(object sender, EventArgs e)
     {
-        //Delete on server3
+        //Delete test on server3
         using (var context = new db_testsEntities_server3())
         {
             try
@@ -92,6 +90,7 @@ public partial class _Default : System.Web.UI.Page
                 context.server3_table_tests.Remove(test_row);
                 context.SaveChanges();
                 Label2.Text = "Succeded";
+                Label5.Text = "";
                 using (var context_2 = new db_testsEntities_server4())
                 {
                     ///////////////////////////////////////////////////////////////////
@@ -103,7 +102,107 @@ public partial class _Default : System.Web.UI.Page
             }
             catch (Exception exep)
             {
-                Label2.Text = "Fail to delete: "+exep;
+                Label2.Text = "Failed";
+                Label5.Text = "Fail to delete: "+exep;
+            }
+        }
+    }
+    protected void Button3_Click(object sender, EventArgs e)
+    {
+        // insert test to server4
+        // get the name of the test for server4
+        string TestNameServer4 = TextBox2.Text;
+        using (var context = new db_testsEntities_server4())
+        {
+            var test_row = new server4_table_tests { TestName = TestNameServer4 };
+            try
+            {
+                ///////////////////////////////////////////////////////////////////
+                // This code allows you to insert a row in a table
+                ///////////////////////////////////////////////////////////////////
+                context.server4_table_tests.Add(test_row);
+                context.SaveChanges();
+                Label3.Text = "Succeded";
+                Label5.Text = "";
+                using (var context_2 = new db_testsEntities_server4())
+                {
+                    ///////////////////////////////////////////////////////////////////
+                    // This code allows you to select a View from you DataBase
+                    ///////////////////////////////////////////////////////////////////
+                    GridView1.DataSource = context_2.view_table_tests.ToList();
+                    GridView1.DataBind();
+                }
+            }
+            catch (Exception exep)
+            {
+                Label3.Text = "Failed";
+                Label5.Text = "Fail to insert: " + exep;
+            }
+
+        }
+    }
+    protected void Button4_Click(object sender, EventArgs e)
+    {
+        //Delete test on server4
+        using (var context = new db_testsEntities_server4())
+        {
+            try
+            {
+                // get the name of the test to delete
+                int TestNameServer4 = Convert.ToInt32(TextBox3.Text);
+                //Get the row to delete
+                var test_row = new server4_table_tests { TestID = TestNameServer4 };
+                /////////////////////////////////////////////////////////////
+                // This code allows us to delete a row
+                /////////////////////////////////////////////////////////////
+                //http://stackoverflow.com/questions/17723276/delete-a-single-record-from-entity-framework
+                context.server4_table_tests.Attach(test_row);
+                context.server4_table_tests.Remove(test_row);
+                context.SaveChanges();
+                Label4.Text = "Succeded";
+                Label5.Text = "";
+                using (var context_2 = new db_testsEntities_server4())
+                {
+                    ///////////////////////////////////////////////////////////////////
+                    // This code allows you to select a View from you DataBase
+                    ///////////////////////////////////////////////////////////////////
+                    GridView1.DataSource = context_2.view_table_tests.ToList();
+                    GridView1.DataBind();
+                }
+            }
+            catch (Exception exep)
+            {
+                Label4.Text = "Failed";
+                Label5.Text = "Fail to delete: " + exep;
+            }
+        }
+    }
+    protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+    {  
+        GridView1.Rows[0].Cells[0].BackColor = System.Drawing.Color.Yellow;
+    }
+    protected void GridView1_RowDataBound(object sender, GridViewRowEventArgs e)
+    {
+        //GridView1.Rows[0].Cells[0].BackColor = System.Drawing.Color.Yellow;
+    }
+    protected void GridView1_DataBound(object sender, EventArgs e)
+    {
+        //To color up the cells with a different color depending on the server
+        for (int i = 0; i <= GridView1.Rows.Count - 1; i++)
+        {
+            if (Convert.ToInt32(GridView1.Rows[i].Cells[0].Text) < 6)
+            {
+                var color_server3 = System.Drawing.Color.AntiqueWhite;
+                GridView1.Rows[i].Cells[0].BackColor = color_server3;
+                TextBox1.BackColor = color_server3;
+                TextBox4.BackColor = color_server3;
+            }
+            else
+            {
+                var color_server4 = System.Drawing.Color.Coral;
+                GridView1.Rows[i].Cells[0].BackColor = color_server4;
+                TextBox2.BackColor = color_server4;
+                TextBox3.BackColor = color_server4;
             }
         }
     }
