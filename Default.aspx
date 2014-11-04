d2a8d3f8348b3a5d978e6c3d738e7eb2f717abb9
cniackz <cesar.celis.h@gmail.com>
2014-11-04 03:03:50 -0600

    my_web_app

---
+++ Default.aspx
@@ -0,0 +1,68 @@
+﻿
+<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            font-size: large;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <strong><span class="auto-style1">Server3 and Server4 </span></strong>
+        <br />
+        <br />
+        1. Display tests<br />
+    
+        <asp:GridView ID="GridView1" runat="server" OnSelectedIndexChanged="GridView1_SelectedIndexChanged" style="width: 187px">
+        </asp:GridView>
+    
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Refresh Table" />
+        <br />
+        <br />
+        <strong><span class="auto-style1">Server3</span></strong><br />
+        <br />
+        1. Insert new test<br />
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <br />
+        <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Insert Server3" />
+        <br />
+        Result:
+        <asp:Label ID="Label1" runat="server"></asp:Label>
+        <br />
+        <br />
+        2. Delete a test<br />
+        <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+        <br />
+        <asp:Button ID="Button5" runat="server" OnClick="Button5_Click" Text="Delete Server3" />
+        <br />
+        Result:
+        <asp:Label ID="Label2" runat="server"></asp:Label>
+        <br />
+        <br />
+        <strong><span class="auto-style1">Server4</span></strong><br />
+        <br />
+        1. Insert new test<br />
+        <asp:TextBox ID="TextBox2" runat="server" OnTextChanged="TextBox2_TextChanged"></asp:TextBox>
+        <br />
+        <asp:Button ID="Button3" runat="server" Text="Insert Server4" />
+        <br />
+        <br />
+        2. Delete a test<br />
+        <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+        <br />
+        <asp:Button ID="Button4" runat="server" Text="Delete Server4" />
+        <br />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        using ( var context = new db_testsEntities_server4() ) { 
+            ///////////////////////////////////////////////////////////////////
+            // This code allows you to select a View from you DataBase
+            ///////////////////////////////////////////////////////////////////
+            GridView1.DataSource = context.view_table_tests.ToList();
+            GridView1.DataBind();
+        }
+
+    }
+    protected void Unnamed1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
+    protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        using (var context = new db_testsEntities_server4())
+        {
+            ///////////////////////////////////////////////////////////////////
+            // This code allows you to select a View from you DataBase
+            ///////////////////////////////////////////////////////////////////
+            GridView1.DataSource = context.view_table_tests.ToList();
+            GridView1.DataBind();
+        }
+    }
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        //We will insert a test to server3
+        // get the name of the test for server3
+        string TestNameServer3 = TextBox1.Text;
+        using (var context = new db_testsEntities_server3())
+        {
+            var test_row = new server3_table_tests { TestName = TestNameServer3 };
+            try { 
+                ///////////////////////////////////////////////////////////////////
+                // This code allows you to insert a row in a table
+                ///////////////////////////////////////////////////////////////////
+                context.server3_table_tests.Add(test_row);
+                context.SaveChanges();
+                Label1.Text = "Succeded";
+                using (var context_2 = new db_testsEntities_server4())
+                {
+                    ///////////////////////////////////////////////////////////////////
+                    // This code allows you to select a View from you DataBase
+                    ///////////////////////////////////////////////////////////////////
+                    GridView1.DataSource = context_2.view_table_tests.ToList();
+                    GridView1.DataBind();
+                }
+            }
+            catch (Exception exep) 
+            {
+                Label1.Text = "Fail to insert: "+exep;
+            }
+
+        }
+    }
+    protected void TextBox2_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button5_Click(object sender, EventArgs e)
+    {
+        //Delete on server3
+        using (var context = new db_testsEntities_server3())
+        {
+            try
+            {
+                // get the name of the test to delete
+                int TestNameServer3 = Convert.ToInt32(TextBox4.Text);
+                //Get the row to delete
+                var test_row = new server3_table_tests { TestID = TestNameServer3 };
+                /////////////////////////////////////////////////////////////
+                // This code allows us to delete a row
+                /////////////////////////////////////////////////////////////
+                //http://stackoverflow.com/questions/17723276/delete-a-single-record-from-entity-framework
+                context.server3_table_tests.Attach(test_row);
+                context.server3_table_tests.Remove(test_row);
+                context.SaveChanges();
+                Label2.Text = "Succeded";
+                using (var context_2 = new db_testsEntities_server4())
+                {
+                    ///////////////////////////////////////////////////////////////////
+                    // This code allows you to select a View from you DataBase
+                    ///////////////////////////////////////////////////////////////////
+                    GridView1.DataSource = context_2.view_table_tests.ToList();
+                    GridView1.DataBind();
+                }
+            }
+            catch (Exception exep)
+            {
+                Label2.Text = "Fail to delete: "+exep;
+            }
+        }
+    }
+}
